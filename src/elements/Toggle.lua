58255957a1f622e5ab9a9c12ddbad0531fc5b66f
danuuww <tdanu8@gmail.com>
2026-03-19 20:26:41 +0700

    Add IsPointerInside function and refactor Toggle

--- src/elements/Toggle.lua
+++ src/elements/Toggle.lua
@@ -7,6 +7,21 @@ local CreateCheckbox = require("../components/ui/Checkbox").New
 
 local Element = {}
 
+local function IsPointerInside(Object, Input)
+	if not Object or not Input then
+		return false
+	end
+
+	local Pos = Object.AbsolutePosition
+	local Size = Object.AbsoluteSize
+	local Point = Input.Position
+
+	return Point.X >= Pos.X
+		and Point.X <= (Pos.X + Size.X)
+		and Point.Y >= Pos.Y
+		and Point.Y <= (Pos.Y + Size.Y)
+end
+
 function Element:New(Config)
 	local Toggle = {
 		__type = "Toggle",
@@ -16,25 +31,23 @@ function Element:New(Config)
 		LockedTitle = Config.LockedTitle,
 		Value = Config.Value,
 		Icon = Config.Icon or nil,
-		IconSize = Config.IconSize or 23,
+		IconSize = Config.IconSize or 20,
 		Type = Config.Type or "Toggle",
 		Callback = Config.Callback or function() end,
 		UIElements = {}
 	}
 
 	local HasDesc = Toggle.Desc ~= nil and Toggle.Desc ~= ""
-	local ToggleSlotWidth = Config.Window.NewElements and 108 or 92
-	local ChevronWidth = HasDesc and 18 or 0
-	local RightSlotWidth = ToggleSlotWidth + ChevronWidth + (HasDesc and 8 or 0)
 
 	Toggle.ToggleFrame = require("../components/window/Element")({
 		Title = Toggle.Title,
 		Desc = Toggle.Desc,
 		Image = Toggle.Icon,
-		ImageSize = Toggle.Icon and Toggle.IconSize or 0,
+		ImageSize = Toggle.Icon and math.clamp(Toggle.IconSize or 20, 18, 22) or nil,
+		IconThemed = Config.IconThemed,
 		Window = Config.Window,
 		Parent = Config.Parent,
-		TextOffset = RightSlotWidth + 8,
+		TextOffset = HasDesc and 138 or 126,
 		Hover = false,
 		Tab = Config.Tab,
 		Index = Config.Index,
@@ -44,9 +57,7 @@ function Element:New(Config)
 		ExpandableDesc = HasDesc,
 		DescExpanded = false,
 		ShowChevron = HasDesc,
-		RightSlotWidth = RightSlotWidth,
-		DividerLeftInset = Toggle.Icon and (Toggle.IconSize + 42) or 22,
-		DividerRightInset = RightSlotWidth + 14,
+		RightSlotWidth = HasDesc and 118 or 92,
 	})
 
 	local CanCallback = true
@@ -72,13 +83,14 @@ function Element:New(Config)
 	end
 
 	local Toggled = Toggle.Value
+	local ToggleFrame
+	local ToggleFunc
 
-	local ToggleFrame, ToggleFunc
 	if Toggle.Type == "Toggle" then
 		ToggleFrame, ToggleFunc = CreateToggle(
 			Toggled,
 			nil,
-			0,
+			nil,
 			Toggle.ToggleFrame.UIElements.RightSlot,
 			Toggle.Callback,
 			Config.Window.NewElements,
@@ -88,7 +100,7 @@ function Element:New(Config)
 		ToggleFrame, ToggleFunc = CreateCheckbox(
 			Toggled,
 			nil,
-			0,
+			nil,
 			Toggle.ToggleFrame.UIElements.RightSlot,
 			Toggle.Callback,
 			Config
@@ -97,10 +109,35 @@ function Element:New(Config)
 		error("Unknown Toggle Type: " .. tostring(Toggle.Type))
 	end
 
-	Toggle.UIElements.Control = ToggleFrame
+	Toggle.UIElements.Switch = ToggleFrame
 
-	ToggleFrame.AnchorPoint = Vector2.new(1, 0.5)
-	ToggleFrame.Position = UDim2.new(1, HasDesc and -(ChevronWidth + 8) or 0, 0.5, 0)
+	ToggleFrame.AnchorPoint = Vector2.new(0, 0.5)
+	ToggleFrame.Position = UDim2.new(0, 0, 0.5, 0)
+	ToggleFrame.LayoutOrder = 1
+	ToggleFrame.ZIndex = 5
+
+	local ToggleScale = New("UIScale", {
+		Scale = Toggle.Type == "Toggle" and 0.90 or 0.92,
+		Parent = ToggleFrame,
+	})
+
+	Toggle.UIElements.ToggleScale = ToggleScale
+
+	local ChevronButton = Toggle.ToggleFrame.UIElements.ChevronButton
+
+	local function RefreshRightSlot()
+		local Width = ToggleFrame.AbsoluteSize.X
+		if Width <= 0 then
+			Width = Toggle.Type == "Toggle" and 84 or 28
+		end
+
+		local ExtraChevron = ChevronButton and ChevronButton.Parent and ChevronButton.Parent.AbsoluteSize.X or 0
+		local Gap = ChevronButton and 8 or 0
+
+		Toggle.ToggleFrame.RightSlotWidth = Width + ExtraChevron + Gap
+	end
+
+	task.defer(RefreshRightSlot)
 
 	function Toggle:Set(v, isCallback, isAnim)
 		if CanCallback then
@@ -112,30 +149,58 @@ function Element:New(Config)
 
 	Toggle:Set(Toggled, false, Config.Window.NewElements)
 
+	local function ShouldIgnoreMainPress(Input)
+		if ChevronButton and IsPointerInside(ChevronButton, Input) then
+			return true
+		end
+		return false
+	end
+
 	if Config.Window.NewElements and ToggleFunc.Animate then
-		Creator.AddSignal(Toggle.ToggleFrame.UIElements.Main.InputBegan, function(input)
+		Creator.AddSignal(Toggle.ToggleFrame.UIElements.Main.InputBegan, function(Input)
 			if Toggle.Locked then
 				return
 			end
 
-			if Toggle.ToggleFrame.UIElements.ChevronButton then
-				local chevronAbsPos = Toggle.ToggleFrame.UIElements.ChevronButton.AbsolutePosition
-				local chevronAbsSize = Toggle.ToggleFrame.UIElements.ChevronButton.AbsoluteSize
-
-				if input.Position.X >= chevronAbsPos.X and input.Position.X <= chevronAbsPos.X + chevronAbsSize.X
-					and input.Position.Y >= chevronAbsPos.Y and input.Position.Y <= chevronAbsPos.Y + chevronAbsSize.Y then
-					return
-				end
+			if ShouldIgnoreMainPress(Input) then
+				return
 			end
 
-			if (not Config.Window.IsToggleDragging)
-				and (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
-				ToggleFunc:Animate(input, Toggle)
+			if not Config.Window.IsToggleDragging
+				and (Input.UserInputType == Enum.UserInputType.MouseButton1 or Input.UserInputType == Enum.UserInputType.Touch) then
+				ToggleFunc:Animate(Input, Toggle)
 			end
 		end)
 	else
 		Creator.AddSignal(Toggle.ToggleFrame.UIElements.Main.MouseButton1Click, function()
+			if Toggle.Locked then
+				return
+			end
 			Toggle:Set(not Toggle.Value, nil, Config.Window.NewElements)
+		end)
+	end
+
+	Creator.AddSignal(ToggleFrame:GetPropertyChangedSignal("AbsoluteSize"), function()
+		RefreshRightSlot()
+	end)
+
+	if ChevronButton then
+		Creator.AddSignal(ChevronButton.MouseEnter, function()
+			local Icon = Toggle.ToggleFrame.UIElements.ChevronIcon
+			if Icon then
+				Tween(Icon, 0.12, {
+					TextTransparency = 0.08,
+				}, Enum.EasingStyle.Quint, Enum.EasingDirection.Out):Play()
+			end
+		end)
+
+		Creator.AddSignal(ChevronButton.MouseLeave, function()
+			local Icon = Toggle.ToggleFrame.UIElements.ChevronIcon
+			if Icon then
+				Tween(Icon, 0.12, {
+					TextTransparency = 0.25,
+				}, Enum.EasingStyle.Quint, Enum.EasingDirection.Out):Play()
+			end
 		end)
 	end
 
